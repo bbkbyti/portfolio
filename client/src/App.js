300c4af50fdd5f0edc440ef5eeba2616eb03e80d
Babak Bayati <bbkbyti@Babaks-MacBook-Pro.local>
2021-08-24 12:35:06 -0500

    designing about/personalInfo section

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Data from './components/Data';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import Skills from './components/Skills';
+// import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Data />
-      <Skills />
+      {/* <Skills /> */}
       <Footer />
     </div>
   );
--- client/src/components/Data.jsx
+++ client/src/components/Data.jsx
@@ -1,5 +1,7 @@
 import { getData } from '../services';
 import { useState, useEffect } from 'react'
+import Skills from './Skills';
+import './Data.css'
 
 
 
@@ -17,20 +19,49 @@ export default function Data() {
 
 
     return (
-        <div>
+        <div className='page'>
             {data.map((dat) => {
                 return (
-                    <div>
-                        <img src={dat.fields.pic} alt={dat.fields.name}/>
-                        <p>{dat.fields.name}</p>
-                        <p>{dat.fields.last}</p>
-                        <p>{dat.fields.email}</p>
-                        <p>{dat.fields.phone}</p>
-                        <p>{dat.fields.address}</p>
+                    <div className='row'>
+                        <div className='about'><span id='aboutMe'>About Me</span><br/>{dat.fields.about}</div>  
+                        <div className='column'>
+                            <div className='container'>
+                                <img className='pic' src={dat.fields.pic} alt={dat.fields.name} />
+                                <div id='title'><p>{dat.fields.title}</p></div>
+                                <div className='info'>
+                                <p>{dat.fields.name}</p>
+                                <p>{dat.fields.email}</p>
+                                <p>{dat.fields.phone}</p>
+                                <p>{dat.fields.address}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='column'>
+                            
+                            <div className='skills-card'>
+                                <div className='skills'><span id='sk'>Skills</span></div>
+                                <p>
+                                    <span id='f'>Frontend Development</span>
+                                    <br/>
+                                {dat.fields.frontend}</p>
+                                
+                                <p>
+                                    <span id='f'>Backend Development</span>
+                                    <br/>
+                                {dat.fields.backend}</p>
+                                <p>
+                                    <span id='f'>API Development</span>
+                                    <br/>
+                                    {dat.fields.api}</p>
+                                
+                            </div>
+                        </div>
+                        <div>
+                            <h1>Projects</h1>
+                        </div>
                     </div>
                 )
             })}
-
         </div>
     )
 }
