c8346b77b78fa68a5f9c878ab664c1d4ce070e49
Babak Bayati <bbkbyti@Babaks-MBP.lan>
2021-09-10 13:21:22 -0500

    added a home icon to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FaHome } from "react-icons/fa";
 
 
 export default function Navbar() {
     return (
         <div className=" py-8 font-serif text-white bg-gray-700 flex justify-evenly" >
+
             <div>
-                <p>Contact</p>
+                <Link to='/'>
+                <FaHome size={25} />
+                </Link>
             </div>
+
             <div>
                 <p>Projects</p>
             </div>
